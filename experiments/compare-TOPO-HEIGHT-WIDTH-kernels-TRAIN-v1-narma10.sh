f394bc153a1cf463a440cab7abaef91481002b1f
Filip Matzner <floop@floop.cz>
2023-03-26 23:47:20 +0200

    Add kernel list to output dir

--- experiments/compare-TOPO-HEIGHT-WIDTH-kernels-TRAIN-v1-narma10.sh
+++ experiments/compare-TOPO-HEIGHT-WIDTH-kernels-TRAIN-v1-narma10.sh
@@ -4,13 +4,14 @@ TOPO="$1"
 HEIGHT="$2"
 WIDTH="$3"
 TRAIN="$4"
+KERNELS=${KERNELS:-"3 5 7 11 15 19"}
 
-# Name output folder using kenels (replace space with comma) 
-outdir="./log/compare-${TOPO}-${HEIGHT}-${WIDTH}-kernels-train${TRAIN}-v1-narma10/"
+kernels_str="k$(echo "${KERNELS}" | sed -e 's/ \+/k/g')"
+outdir="./log/compare-${TOPO}-${HEIGHT}-${WIDTH}-${kernels_str}-train${TRAIN}-v1-narma10/"
 mkdir -p "${outdir}"
 ./build/compare_lcnn_kernels_cuda \
   --gen.net-type=lcnn \
-  --gen.kernel-sizes 3 5 7 11 15 19 \
+  --gen.kernel-sizes=${KERNELS} \
   --lcnn.topology="${TOPO}" \
   --lcnn.input-to-all=true \
   --gen.state-heights="${HEIGHT}" \
