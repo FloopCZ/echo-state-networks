f5fa978f58e5bc5fb842b8475086d8b58f6e7e0a
Filip Matzner <floop@floop.cz>
2023-04-17 23:52:08 +0200

    Test larger kernels

--- test/lcnn_step_cuda_test.cpp
+++ test/lcnn_step_cuda_test.cpp
@@ -7,8 +7,8 @@ TEST(LcnnStepCudaTest, ConstTest)
     int state_height = 100;
     int state_width = 200;
     af::array state = af::constant(3, state_height, state_width, af::dtype::f64);
-    for (int kernel_height = 3; kernel_height < 10; kernel_height += 2) {
-        for (int kernel_width = 3; kernel_width < 10; kernel_width += 2) {
+    for (int kernel_height = 3; kernel_height < 30; kernel_height += 2) {
+        for (int kernel_width = 3; kernel_width < 30; kernel_width += 2) {
             af::array reservoir_w = af::constant(
               5, state_height, state_width, kernel_height, kernel_width, af::dtype::f64);
             af::array new_state = lcnn_step(state, reservoir_w);
