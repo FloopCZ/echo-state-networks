fbed02c37fec708a3ebfac3b7d14813c0fc5f548
Filip Matzner <floop@floop.cz>
2024-04-13 18:29:23 +0200

    Add ettm cluster experiments

---
+++ experiments/compare-TOPO-HEIGHT-WIDTH-kernels-ettm1-AHEAD-loop.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ $# != 4 ]; then echo "Invalid usage"; exit 1; fi
+TOPO="$1"
+HEIGHT="$2"
+WIDTH="$3"
+AHEAD="$4"
+KERNELS=${KERNELS:-"3 5 7 11 15"}
+TASK_OFFSET=${TASK_OFFSET:-0}
+N_TASKS=${N_TASKS:-99999}
+
+export AF_MAX_BUFFERS=100000
+kernels_str="k$(echo "${KERNELS}" | sed -e 's/ \+/k/g')"
+outdir="./log/compare-${TOPO}-${HEIGHT}-${WIDTH}-${kernels_str}-ettm1-ahead${AHEAD}-loop/"
+mkdir -p "${outdir}"
+./build/compare_lcnn_kernels_cuda \
+  --gen.net-type=lcnn \
+  --gen.kernel-sizes=${KERNELS} \
+  --gen.optimizer-type=lcnn \
+  --opt.exclude-params=default \
+  --opt.exclude-params=lcnn.sigma-fb-weight \
+  --lcnn.mu-in-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.mu-fb-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.sigma-fb-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.topology="${TOPO}" \
+  --lcnn.state-height="${HEIGHT}" \
+  --lcnn.state-width="${WIDTH}" \
+  --lcnn.memory-length=60 \
+  --lcnn.memory-prob=1 \
+  --gen.benchmark-set=ettm-loop \
+  --bench.etth-variant=1 \
+  --bench.ett-set-type=train-valid \
+  --bench.init-steps=500 \
+  --bench.train-steps=34060 \
+  --bench.valid-steps=11520 \
+  --bench.n-steps-ahead="${AHEAD}" \
+  --bench.validation-stride=30 \
+  --gen.n-trials=1 \
+  --gen.n-runs=1 \
+  --gen.af-device=0 \
+  --gen.output-dir="${outdir}" \
+  --gen.task-offset="${TASK_OFFSET}" \
+  --gen.n-tasks="${N_TASKS}" \
+  2>&1 | tee -a "${outdir}/out_${TASK_OFFSET}_${N_TASKS}.txt"
---
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ $# != 6 ]; then echo "Invalid usage"; exit 1; fi
+TOPO="$1"
+HEIGHT="$2"
+WIDTH="$3"
+AHEAD="$4"
+MEMLEN="$5"
+MEMPROB="$6"
+
+export AF_MAX_BUFFERS=100000
+outdir="./log/optimize-${TOPO}-${HEIGHT}-${WIDTH}-ettm1-ahead${AHEAD}-memlen${MEMLEN}-memprob${MEMPROB}-loop/"
+mkdir -p "${outdir}"
+./build/optimize_cuda \
+  --gen.net-type=lcnn \
+  --gen.optimizer-type=lcnn \
+  --opt.exclude-params=default \
+  --opt.exclude-params=lcnn.sigma-fb-weight \
+  --lcnn.mu-in-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.mu-fb-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.sigma-fb-weight=0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
+  --lcnn.topology="${TOPO}" \
+  --lcnn.state-height="${HEIGHT}" \
+  --lcnn.state-width="${WIDTH}" \
+  --lcnn.memory-length="${MEMLEN}" \
+  --lcnn.memory-prob="${MEMPROB}" \
+  --gen.benchmark-set=ettm-loop \
+  --bench.etth-variant=1 \
+  --bench.ett-set-type=train-valid \
+  --bench.init-steps=500 \
+  --bench.train-steps=34060 \
+  --bench.valid-steps=11520 \
+  --bench.n-steps-ahead="${AHEAD}" \
+  --bench.validation-stride=30 \
+  --gen.n-trials=1 \
+  --gen.n-runs=1 \
+  --gen.af-device=0 \
+  --gen.output-dir="${outdir}" \
+  2>&1 | tee -a "${outdir}/out.txt"
