91bc21c7f01c609d8a39a09dc2c02a2244c76925
Filip Matzner <floop@floop.cz>
2024-04-13 12:51:34 +0200

    Archive old experiments

---
+++ experiments/archive/README.md
@@ -0,0 +1,4 @@
+# Experiments from previous papers
+
+The experiments in this folder may not work with the latest version
+of this software and are here mostly for a reference or a reminder.
--- experiments/optimize-TOPO-HEIGHT-WIDTH-etth1-AHEAD-loop.sh
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-etth1-AHEAD-loop.sh
@@ -32,7 +32,6 @@ mkdir -p "${outdir}"
   --bench.valid-steps=2880 \
   --bench.n-steps-ahead="${AHEAD}" \
   --bench.validation-stride=10 \
-  --opt.max-fevals=3500 \
   --gen.n-trials=1 \
   --gen.n-runs=1 \
   --gen.af-device=0 \
