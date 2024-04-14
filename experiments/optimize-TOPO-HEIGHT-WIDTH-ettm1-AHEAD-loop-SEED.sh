e6d10fda415b82cd0dffc51c4ff510e3b41ba2da
Filip Matzner <floop@floop.cz>
2024-04-14 17:57:29 +0200

    Add seed parameter to experiments

--- experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-loop-SEED.sh
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-loop-SEED.sh
@@ -2,14 +2,15 @@
 set -e
 set -o pipefail
 
-if [ $# != 4 ]; then echo "Invalid usage"; exit 1; fi
+if [ $# -lt 4 ]; then echo "Invalid usage"; exit 1; fi
 TOPO="$1"
 HEIGHT="$2"
 WIDTH="$3"
 AHEAD="$4"
+SEED="${5:-50}"
 
 export AF_MAX_BUFFERS=100000
-outdir="./log/optimize-${TOPO}-${HEIGHT}-${WIDTH}-ettm1-ahead${AHEAD}-loop/"
+outdir="./log/optimize-${TOPO}-${HEIGHT}-${WIDTH}-ettm1-ahead${AHEAD}-loop-seed${SEED}/"
 mkdir -p "${outdir}"
 ./build/optimize_cuda \
   --gen.net-type=lcnn \
@@ -32,6 +33,7 @@ mkdir -p "${outdir}"
   --bench.valid-steps=11520 \
   --bench.n-steps-ahead="${AHEAD}" \
   --bench.validation-stride=30 \
+  --gen.seed="${SEED}" \
   --gen.n-trials=1 \
   --gen.n-runs=1 \
   --gen.af-device=0 \
