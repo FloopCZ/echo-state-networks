d281321dc11cbc6efb904bf48aad5b0d8af152b1
Filip Matzner <floop@floop.cz>
2024-04-17 23:59:20 +0200

    Short forecasting length for memory grid

--- experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-memgrid-loop-cluster.sh
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-memgrid-loop-cluster.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 set -e
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 240 0.5
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 240 1.0
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 15 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 15 1.0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 120 0.5
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 120 1.0
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 30 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 30 1.0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 60 0.5
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 60 1.0
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 60 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 60 1.0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 30 0.5
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 30 1.0
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 120 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 120 1.0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 15 0.5
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 15 1.0
 
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 240 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 240 1.0
-
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 480 0.5
-./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 336 480 1.0
+./experiments/optimize-TOPO-HEIGHT-WIDTH-ettm1-AHEAD-MEMLEN-MEMPROB-loop-cluster.sh lcnn 40 50 192 0 0.0
