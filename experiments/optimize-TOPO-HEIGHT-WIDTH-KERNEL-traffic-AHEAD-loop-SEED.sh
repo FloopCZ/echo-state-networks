1fd63b1ab6aeaddb7d25ebd50129ff92ce4d0038
Filip Matzner <floop@floop.cz>
2024-05-08 18:52:22 +0200

    Add traffic experiment

---
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-traffic-AHEAD-loop-SEED.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ $# -lt 5 ]; then echo "Invalid usage"; exit 1; fi
+TOPO="$1"
+HEIGHT="$2"
+WIDTH="$3"
+KERNEL="$4"
+AHEAD="$5"
+SEED="${6:-50}"
+TASK_OFFSET=${TASK_OFFSET:-0}
+N_TASKS=${N_TASKS:-99999}
+
+export AF_MAX_BUFFERS=100000
+outdir="./log/optimize-${TOPO}-${HEIGHT}-${WIDTH}-k${KERNEL}-traffic-ahead${AHEAD}-loop-seed${SEED}/"
+mkdir -p "${outdir}"
+./build/optimize_cuda \
+  --gen.net-type=lcnn \
+  --gen.optimizer-type=lcnn \
+  --opt.exclude-params=default \
+  --opt.exclude-params=lcnn.sigma-fb-weight \
+  --lcnn.topology="${TOPO}" \
+  --lcnn.state-height="${HEIGHT}" \
+  --lcnn.state-width="${WIDTH}" \
+  --lcnn.kernel-height="${KERNEL}" \
+  --lcnn.kernel-width="${KERNEL}" \
+  --lcnn.memory-length=100 \
+  --gen.benchmark-set=traffic-loop \
+  --bench.ett-variant=1 \
+  --bench.set-type=train-valid \
+  --bench.init-steps=500 \
+  --bench.train-steps=11780 \
+  --bench.valid-steps=3508 \
+  --bench.n-steps-ahead="${AHEAD}" \
+  --bench.validation-stride=30 \
+  --gen.seed="${SEED}" \
+  --gen.n-trials=1 \
+  --gen.af-device=0 \
+  --gen.output-dir="${outdir}" \
+  --gen.task-offset="${TASK_OFFSET}" \
+  --gen.n-tasks="${N_TASKS}" \
+  2>&1 | tee -a "${outdir}/out_${TASK_OFFSET}_${N_TASKS}.txt"
