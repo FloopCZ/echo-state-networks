24c098050c09165b3b44103ae2079aec09c5ee0a
Filip Matzner <floop@floop.cz>
2024-05-10 12:43:46 +0200

    Remove extra subfolder

--- experiments/evaluate-test-real-world-datasets.sh
+++ experiments/evaluate-test-real-world-datasets.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 
 for ds in electricity etth1 etth2 exchange solar traffic weather ettm1 ettm2; do
-    log_dir="./log/real-world-datasets/optimize-lcnn-40-50-k7-${ds}-ahead192-loop-seed50"
+    log_dir="./log/optimize-lcnn-40-50-k7-${ds}-ahead192-loop-seed50"
     best_run=$(./scripts/best_run.py "${log_dir}"/optimization_results_*.csv)
     echo "Evaluating ${ds}, run ${best_run}"
     "./experiments/evaluate-${ds}-loop-test-MODEL-AUTORETRAIN.sh" "${log_dir}/run${best_run}/best-model" 0 192 1 
