993856439381ff26bccd71f72e0faa49c406149b
Filip Matzner <floop@floop.cz>
2024-05-17 22:06:25 +0200

    Reorder experiments in optimization script

--- experiments/optimize-real-world-datasets-cluster.sh
+++ experiments/optimize-real-world-datasets-cluster.sh
@@ -2,6 +2,6 @@
 set -e
 set -o pipefail
 
-for ds in electricity etth1 etth2 exchange solar traffic weather ettm1 ettm2; do
+for ds in ettm1 ettm2 solar traffic electricity weather exchange etth1 etth2; do
     "./experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-${ds}-AHEAD-loop-SEED-cluster.sh" lcnn 57 70 7 192 50
 done
