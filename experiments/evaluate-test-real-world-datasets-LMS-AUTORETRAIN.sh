16c2cf460dfc7fc40a223b494d9659b1d219813b
Filip Matzner <floop@floop.cz>
2024-05-19 22:27:08 +0200

    Reorder datasets for evaluation

--- experiments/evaluate-test-real-world-datasets-LMS-AUTORETRAIN.sh
+++ experiments/evaluate-test-real-world-datasets-LMS-AUTORETRAIN.sh
@@ -7,7 +7,7 @@ if [ $# != 2 ]; then echo "Invalid usage"; exit 1; fi
 LMS="${1}"
 AUTORETRAIN_EVERY="${2}"
 
-for ds in electricity etth1 etth2 exchange solar traffic weather ettm1 ettm2; do
+for ds in ettm1 ettm2 solar traffic electricity weather exchange etth1 etth2; do
     for n_steps_ahead in 96 192 336 720; do
         log_dir="./log/optimize-lcnn-40-50-k7-${ds}-ahead192-loop-seed50"
         best_run=$(./scripts/best_run.py "${log_dir}"/optimization_results_*.csv)
