42c27a1c6cecb323b7cdcf1e1fd1d902e4eb28fc
Filip Matzner <floop@floop.cz>
2024-09-19 17:28:44 +0200

    Add ettm2 loop experiment with memlen0

---
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-ettm2-AHEAD-loop-memlen0-SEED-cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+BACKEND=${BACKEND:-"cuda"}
+
+n_tasks=1
+for task_offset in `seq 0 $n_tasks 4`; do
+    echo $task_offset $n_tasks
+    cmd="./experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-ettm2-AHEAD-loop-memlen0-SEED.sh $@"
+    qsub -v BACKEND="${BACKEND}",TASK_OFFSET="${task_offset}",N_TASKS="${n_tasks}",cmd="${cmd}" ../run-cuda-experiment-singularity.sh
+done
---
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-ettm2-AHEAD-loop-memlen0-SEED.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [ $# -lt 5 ]; then echo "Invalid usage"; exit 1; fi
+TOPO="$1"
+HEIGHT="$2"
+WIDTH="$3"
+KERNEL="$4"
+AHEAD="$5"
+SEED="${6:-50}"
+TASK_OFFSET=${TASK_OFFSET:-0}
+N_TASKS=${N_TASKS:-99999}
+BACKEND=${BACKEND:-"cuda"}
+
+MULTITHREADING="false"
+if [[ "$BACKEND" == "cpu" ]]; then
+    export AF_SYNCHRONOUS_CALLS=1
+    MULTITHREADING="true"
+fi
+
+export AF_MAX_BUFFERS=100000
+out_dir="./log/optimize-${TOPO}-${HEIGHT}-${WIDTH}-k${KERNEL}-ettm2-ahead${AHEAD}-loop-memlen0-seed${SEED}/"
+mkdir -p "${out_dir}"
+"./build/optimize_${BACKEND}" \
+  --gen.net-type=lcnn \
+  --gen.optimizer-type=lcnn \
+  --opt.exclude-params=default \
+  --opt.exclude-params=lcnn.sigma-fb-weight \
+  --lcnn.topology="${TOPO}" \
+  --lcnn.state-height="${HEIGHT}" \
+  --lcnn.state-width="${WIDTH}" \
+  --lcnn.kernel-height="${KERNEL}" \
+  --lcnn.kernel-width="${KERNEL}" \
+  --lcnn.memory-length=0 \
+  --gen.benchmark-set=ettm-loop \
+  --bench.ett-variant=2 \
+  --bench.set-type=train-valid \
+  --bench.init-steps=500 \
+  --bench.train-steps=34059 \
+  --bench.valid-steps=11521 \
+  --bench.n-steps-ahead="${AHEAD}" \
+  --bench.validation-stride=30 \
+  --gen.seed="${SEED}" \
+  --gen.n-trials=1 \
+  --gen.af-device=0 \
+  --opt.multithreading="${MULTITHREADING}" \
+  --gen.output-dir="${out_dir}" \
+  --gen.task-offset="${TASK_OFFSET}" \
+  --gen.n-tasks="${N_TASKS}" \
+  2>&1 | tee -a "${out_dir}/out_${TASK_OFFSET}_${N_TASKS}.txt"
