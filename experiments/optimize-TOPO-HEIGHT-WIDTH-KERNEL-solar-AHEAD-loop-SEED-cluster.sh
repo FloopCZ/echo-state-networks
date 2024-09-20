635a01bcb47166d880278ba948b16646327ae0f0
Filip Matzner <floop@floop.cz>
2024-09-20 21:45:02 +0200

    Propagate EXTRA_STR to cluster experiments

--- experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-solar-AHEAD-loop-SEED-cluster.sh
+++ experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-solar-AHEAD-loop-SEED-cluster.sh
@@ -7,5 +7,5 @@ n_tasks=1
 for task_offset in `seq 0 $n_tasks 4`; do
     echo $task_offset $n_tasks
     cmd="./experiments/optimize-TOPO-HEIGHT-WIDTH-KERNEL-solar-AHEAD-loop-SEED.sh $@"
-    qsub -v BACKEND="${BACKEND}",MEMLEN="${MEMLEN}",TASK_OFFSET="${task_offset}",N_TASKS="${n_tasks}",cmd="${cmd}" "../run-${BACKEND}-experiment-singularity.sh"
+    qsub -v BACKEND="${BACKEND}",MEMLEN="${MEMLEN}",EXTRA_STR="${EXTRA_STR}",TASK_OFFSET="${task_offset}",N_TASKS="${n_tasks}",cmd="${cmd}" "../run-${BACKEND}-experiment-singularity.sh"
 done
