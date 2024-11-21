057bd84a52ee3445b70371ba4eb4d3336eb15538
Filip Matzner <floop@floop.cz>
2024-11-21 19:46:25 +0100

    Add new arguments to compare plot

--- compare_plot.py
+++ compare_plot.py
@@ -39,7 +39,7 @@ def set_log_y(ax):
     ax.grid(which='minor', color='whitesmoke')
     return ax
 
-def log_plot(df, param, color=None, connect_label=None):
+def plot(df, param, plot_type, logscale, color=None, connect_label=None):
     # Print some useful statistics.
     stats_df = df.groupby(["run", param]).agg({'f-value': ['mean','std']})
     stats_df = stats_df.reset_index().sort_values(param)
@@ -56,13 +56,23 @@ def log_plot(df, param, color=None, connect_label=None):
     pprint(pvals)
 
     # Plot the boxplot for the best runs.
-    df["f-value"] = np.log10(df["f-value"])
+    if logscale:
+        df["f-value"] = np.log10(df["f-value"])
 
     palette = None if color else "deep"
-    ax = sns.violinplot(data=df, x=param, y="f-value", color=color, palette=palette, hue=param, zorder=2)
+    if plot_type == "violin":
+        ax = sns.violinplot(data=df, x=param, y="f-value", color=color, palette=palette, hue=param, zorder=2)
+    elif plot_type == "box":
+        ax = sns.boxplot(data=df, x=param, y="f-value", color=color, palette=palette, hue=param, zorder=2)
+    else:
+        raise ValueError(f"Unknown plot type `{plot_type}`.")
+
     if connect_label:
         sns.lineplot(data=df, x=param, y="f-value", color=color, palette=palette,
                      zorder=1, label=connect_label, errorbar=None)
+
+    if logscale:
+        set_log_y(ax)
 
     return ax
 
@@ -76,6 +86,9 @@ def main():
     parser.add_argument("--connect", type=str, help="The parameter by which should the individual violins "
                                                     "be connected using a line.")
     parser.add_argument("--rotate-labels", type=int, help="The rotation of the x-axis labels.")
+    parser.add_argument("--all-runs", action=argparse.BooleanOptionalAction, help="Select all runs, not only the best")
+    parser.add_argument("--plot-type", type=str, help="Plot type (e.g., violin, box.", default="violin")
+    parser.add_argument("--logscale", action=argparse.BooleanOptionalAction, help="Plot in logarithmic scale")
     parser.add_argument("csvs", nargs='+', help="The csvs to be concatenated and plotted.")
     args = parser.parse_args()
 
@@ -91,17 +104,16 @@ def main():
     if args.sort_by:
         df = df.sort_values(args.sort_by)
     if args.connect:
-        # Plot violins.
         connect_unique = df[args.connect].unique()
         for connect_value, color in zip(connect_unique, sns.color_palette("deep", len(connect_unique))):
             df_exp_set = df[df[args.connect] == connect_value]
-            df_best = best_run(df_exp_set, args.param).copy()
-            log_plot(df_best, param=args.param, color=color, connect_label=connect_value)
+            df_best = best_run(df_exp_set, args.param).copy() if not args.all_runs else df_exp_set
+            plot(df_best, param=args.param, plot_type=args.plot_type, logscale=args.logscale,
+                 color=color, connect_label=connect_value)
             plt.legend(title=args.connect)
     else:
-        df_best = best_run(df, args.param).copy()
-        log_plot(df_best, param=args.param)
-    set_log_y(ax=plt.gca())
+        df_best = best_run(df, args.param).copy() if not args.all_runs else df
+        plot(df_best, param=args.param, plot_type=args.plot_type, logscale=args.logscale)
     plt.gca().tick_params(axis='x', labelrotation=args.rotate_labels)
 
     plt.tight_layout()
