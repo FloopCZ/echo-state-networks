eab352ad14b18e2b5704be08618ea21a0335ca1c
Filip Matzner <floop@floop.cz>
2024-11-24 02:13:35 +0100

    Add Timer class

--- misc.hpp
+++ misc.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <arrayfire.h>
+#include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <range/v3/all.hpp>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace esn {
@@ -36,5 +43,55 @@ long index_of(const T& value, const std::vector<T>& keys)
 {
     return std::distance(keys.begin(), std::find(keys.begin(), keys.end(), value));
 }
+
+class Timer {
+private:
+    struct TimeRecord {
+        std::chrono::steady_clock::time_point start_time;
+        std::chrono::duration<double> duration;
+        bool is_running;
+    };
+
+    std::unordered_map<std::string, TimeRecord> records_;
+    std::ostream* stream_output_;
+    std::ofstream csv_file_;
+
+public:
+    Timer(std::ostream& output = std::cout, const std::string& csv_path = "")
+      : stream_output_(&output)
+    {
+        if (!csv_path.empty()) {
+            csv_file_.open(csv_path, std::ios::out | std::ios::app);
+            if (csv_file_.is_open())
+                csv_file_ << "Label,Duration (seconds)\n";
+            else
+                throw std::ios_base::failure("Failed to open CSV file.");
+        }
+    }
+
+    void start(const std::string& label)
+    {
+        if (records_[label].is_running)
+            throw std::logic_error("Benchmark already started for label: " + label);
+        records_[label].start_time = std::chrono::steady_clock::now();
+        records_[label].is_running = true;
+    }
+
+    void stop(const std::string& label)
+    {
+        auto it = records_.find(label);
+        if (it == records_.end() || !it->second.is_running)
+            throw std::logic_error("Benchmark not started for label: " + label);
+
+        auto& record = it->second;
+        record.duration = std::chrono::steady_clock::now() - record.start_time;
+        record.is_running = false;
+
+        *stream_output_ << std::fixed << std::setprecision(6) << "Timer [" << label
+                        << "]: " << record.duration.count() << " seconds." << std::endl;
+
+        if (csv_file_.is_open()) csv_file_ << label << "," << record.duration.count() << "\n";
+    }
+};
 
 }  // namespace esn
