32653d1b611d10106943617903d919a584b59e04
Filip Matzner <floop@floop.cz>
2024-11-26 14:28:32 +0100

    Improve compare benchmark

--- compare_lcnn_step_to_sparse_matmul.cpp
+++ compare_lcnn_step_to_sparse_matmul.cpp
@@ -1,7 +1,7 @@
+#include "arrayfire_utils.hpp"
 #include "lcnn_step.hpp"
 #include "misc.hpp"
 
-#include <af/random.h>
 #include <arrayfire.h>
 #include <iostream>
 
@@ -16,10 +16,11 @@ sparse_variants generate_weights(
 {
     // generate dense reservoir weights
     af::dim4 lcnn_dims{state_height, state_width, kernel_height, kernel_width};
-    af::array lcnn_data = af::randu(lcnn_dims, af::dtype::f64, af_prng);
+    af::array lcnn_data = (af::randu(lcnn_dims, af::dtype::f64, af_prng) + 1e-3) * 1e-3;
 
     // generate sparse reservoir weights
-    std::vector<double> dense_data(state_height * state_width * state_height * state_width);
+    std::vector<double> lcnn_data_vec = af_utils::to_vector(lcnn_data);
+    std::vector<double> dense_data_vec(state_height * state_width * state_height * state_width);
 
     for (int i = 0; i < state_height; ++i) {
         for (int j = 0; j < state_width; ++j) {
@@ -27,19 +28,18 @@ sparse_variants generate_weights(
                 for (int l = 0; l < kernel_width; ++l) {
                     int from_i = (i + k - kernel_height / 2 + state_height) % state_height;
                     int from_j = (j + l - kernel_width / 2 + state_width) % state_width;
-                    int full_index = i + j * state_height
+                    int dense_index = i + j * state_height
                       + (from_i + from_j * state_height) * state_height * state_width;
                     assert(full_index >= 0 && full_index < (long)dense_data.size());
-                    int sparse_index = i + j * state_height + k * state_height * state_width
+                    int lcnn_index = i + j * state_height + k * state_height * state_width
                       + l * state_height * state_width * kernel_height;
                     assert(sparse_index >= 0 && sparse_index < (long)lcnn_data.elements());
-                    dense_data[full_index] += af::flat(lcnn_data)(sparse_index).scalar<double>();
+                    dense_data_vec[dense_index] += lcnn_data_vec[lcnn_index];
                 }
             }
         }
     }
-    af::array dense{state_height * state_width, state_height * state_width, dense_data.data()};
-
+    af::array dense{state_height * state_width, state_height * state_width, dense_data_vec.data()};
     return {lcnn_data, dense, af::sparse(dense, AF_STORAGE_CSR)};
 }
 
@@ -69,40 +69,41 @@ int main(int argc, char* argv[])
         std::cout << std::endl;
 
         af::randomEngine af_prng = af::randomEngine(AF_RANDOM_ENGINE_DEFAULT, 13);
-        const af::array state = af::randu({state_height, state_width}, af::dtype::f64, af_prng);
+        const af::array state =
+          af::randu({state_height, state_width}, af::dtype::f64, af_prng) * 1e-3;
         const sparse_variants data =
           generate_weights(state_height, state_width, kernel_height, kernel_width, af_prng);
 
-        int count = 1000;
+        int count = 10000;
         esn::Timer timer;
 
         {
-            af::array dense_result = af::matmul(data.dense, af::flat(state));
-            dense_result.eval();
+            af::array new_state = af::matmul(data.dense, af::flat(state));
+            new_state.eval();
             timer.start("dense", false, count);
             for (int i = 0; i < count; ++i) {
-                dense_result = af::matmul(data.dense, af::flat(state));
-                dense_result.eval();
+                new_state = af::matmul(data.dense, af::flat(new_state));
+                new_state.eval();
             }
             timer.stop("dense");
         }
         {
-            af::array sparse_csr_result = af::matmul(data.sparse_csr, af::flat(state));
-            sparse_csr_result.eval();
+            af::array new_state = af::matmul(data.sparse_csr, af::flat(state));
+            new_state.eval();
             timer.start("sparse_csr", false, count);
             for (int i = 0; i < count; ++i) {
-                sparse_csr_result = af::matmul(data.sparse_csr, af::flat(state));
-                sparse_csr_result.eval();
+                new_state = af::matmul(data.sparse_csr, af::flat(new_state));
+                new_state.eval();
             }
             timer.stop("sparse_csr");
         }
         {
             timer.start("lcnn", false, count);
-            af::array lcnn_result = lcnn_step(state, data.lcnn_data);
-            lcnn_result.eval();
+            af::array new_state = lcnn_step(state, data.lcnn_data);
+            new_state.eval();
             for (int i = 0; i < count; ++i) {
-                af::array lcnn_result = lcnn_step(state, data.lcnn_data);
-                lcnn_result.eval();
+                new_state = lcnn_step(new_state, data.lcnn_data);
+                new_state.eval();
             }
             timer.stop("lcnn");
         }
