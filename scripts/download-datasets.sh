f4cff69a92c3de1f251b3100c092e17db2d86df8
Filip Matzner <floop@floop.cz>
2024-12-02 23:00:05 +0100

    Add memory vs kernel heatmap

--- scripts/download-datasets.sh
+++ scripts/download-datasets.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Download the datasets used in the experiments.
+
 curl -L "https://drive.usercontent.google.com/download?id=1ieM39R5mpQz55nMub6LWSFmnSF4YJcg7&confirm=xxx" -o /tmp/datasets.tar.gz
 tar -xvzf /tmp/datasets.tar.gz -C ./third_party/
 rm /tmp/datasets.tar.gz
